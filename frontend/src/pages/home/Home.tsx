317b825a95ad96461885a63252dc7e1c2e002c6b
Abenezer <aabenezzer@gmail.com>
2024-11-19 12:00:46 +0300

    Build featured component

--- frontend/src/pages/home/Home.tsx
+++ frontend/src/pages/home/Home.tsx
@@ -1,7 +1,10 @@
+import Featured from '../../components/featured/Featured'
 import './Home.scss'
 const Home = () => {
   return (
-    <div className='home'>Home</div>
+    <div className='home'>
+      <Featured />
+    </div>
   )
 }
 export default Home
